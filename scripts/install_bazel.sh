7bc5a2e3d82be8f2d385d8e87d47c5daad7ef42d
Lucas Kacher <lucas@vsco.co>
2018-05-10 16:58:43 -0400

    fix: ensure install-bazel.sh is travis-ci specific

--- scripts/install_bazel.sh
+++ scripts/install_bazel.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
+#
+# This script installs Bazel for Debian or MacOS for usage in the Travis CI environment.
+#
+# Required config:
+# - CI
+# - TRAVIS
+# - TRAVIS_OS_NAME
+# - BAZEL_VERSION: Bazel version to install
+#
+# Optional configs:
+# - BAZEL_INSTALL: Location to fetch the installers to, default: ${HOME}/bazel/install
+#
 set -e
 
-LOCAL_OS="$(uname)"
+if [ -z "$CI" ] || [ -z "$TRAVIS" ]; then
+    echo "error: this is intended to be run under travis-ci, exiting..."
+    exit 1;
+fi
 
 if [ -z "$BAZEL_VERSION" ]; then
-   BAZEL_VERSION="0.11.1"
+    echo "error: BAZEL_VERSION required, exiting..."
+    exit 1;
 fi
 
 if [ -z "$BAZEL_INSTALL" ]; then
@@ -14,17 +30,19 @@ fi
 mkdir -p $BAZEL_INSTALL
 pushd $BAZEL_INSTALL
 
-if [ "$LOCAL_OS" == "Linux" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     echo "Installing Bazel ${BAZEL_VERSION} for Linux"
-    wget --no-clobber "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel_${BAZEL_VERSION}-linux-x86_64.deb"
-    sudo dpkg -i "bazel_${BAZEL_VERSION}-linux-x86_64.deb"
-elif [ "$LOCAL_OS" == "Darwin" ]; then
+    DEBIAN_PACKAGE_NAME="bazel_${BAZEL_VERSION}-linux-x86_64.deb"
+    wget --no-clobber "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${DEBIAN_PACKAGE_NAME}"
+    sudo dpkg -i ${DEBIAN_PACKAGE_NAME}
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
     echo "Installing Bazel ${BAZEL_VERSION} for Darwin"
-    curl -sLO https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-without-jdk-installer-darwin-x86_64.sh
-    chmod +x bazel-${BAZEL_VERSION}-without-jdk-installer-darwin-x86_64.sh
-    ./bazel-${BAZEL_VERSION}-without-jdk-installer-darwin-x86_64.sh --user
+    DARWIN_INSTALL_SCRIPT=bazel-${BAZEL_VERSION}-without-jdk-installer-darwin-x86_64.sh
+    curl -sLO https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${DARWIN_INSTALL_SCRIPT}
+    chmod +x ${DARWIN_INSTALL_SCRIPT}
+    ./${DARWIN_INSTALL_SCRIPT} --user
 else
-    echo "Unsupported OS"
+    echo "error: unsupported TRAVIS_OS_NAME: ${TRAVIS_OS_NAME}, exiting"
     exit 1;
 fi
 
